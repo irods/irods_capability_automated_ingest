8b8dfe71ea2710b353813d2a939c2af77e998ebf
Hao Xu <xuh@cs.unc.edu>
2018-02-19 14:45:18 -0500

    add example metadata

---
+++ examples/metadata.py
@@ -0,0 +1,27 @@
+from core import Core
+import os
+
+filesystem_mode = 'filesystem::mode'
+
+class event_handler(Core):
+    
+    @staticmethod
+    def post_data_obj_create(hdlr_mod, session, target, path, **options):
+        s = os.stat(path)
+        mode = s.st_mode
+        
+        obj = session.data_objects.get(target)
+        obj.metadata.add(filesystem_mode, str(mode), '')
+        
+
+    @staticmethod
+    def post_data_obj_modify(hdlr_mod, session, target, path, **options):
+        s = os.stat(path)
+        mode = s.st_mode
+        obj = session.data_objects.get(target)
+        obj.metadata[filesystem_mode] = iRODSMeta(filesystem_mode, str(mode))
+
+
+
+
+
