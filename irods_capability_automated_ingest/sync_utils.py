31c64e67047645ecba5819bc6bfabeb45eb7292e
Hao Xu <xuh@email.unc.edu>
2018-06-21 03:07:33 -0400

    connection pool

--- irods_capability_automated_ingest/sync_utils.py
+++ irods_capability_automated_ingest/sync_utils.py
@@ -1,6 +1,6 @@
 from os.path import dirname, basename
 from irods.models import Collection, DataObject
-from redis import StrictRedis
+from redis import StrictRedis, ConnectionPool
 from celery import Celery
 import importlib
 
@@ -20,9 +20,21 @@ def size(session, path, replica_num=None, resc_name=None):
         return int(row[DataObject.size])
 
 
+redis_connection_pool_map = {}
+
+
 def get_redis(config):
     redis_config = config["redis"]
-    return StrictRedis(host=redis_config["host"], port=redis_config["port"], db=redis_config["db"])
+    host = redis_config["host"]
+    port = redis_config["port"]
+    db = redis_config["db"]
+    url = "redis://" + host + ":" + str(port) + "/" + str(db)
+    pool = redis_connection_pool_map.get(url)
+    if pool is None:
+        pool = ConnectionPool(host=host, port=port, db=db)
+        redis_connection_pool_map[url] = pool
+
+    return StrictRedis(connection_pool=pool)
 
 
 def sync_time_key(path):
