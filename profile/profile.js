8402a5beb9a4dc4c2d799e445953076eecb7cf36
Hao Xu <xuh@email.unc.edu>
2018-07-12 16:50:08 -0400

    more profile more 2

--- profile/profile.js
+++ profile/profile.js
@@ -12,7 +12,7 @@ function drawChart(){
 }
 
 function getMinAndMaxDate() {
-    var index = document.getElementById('index').value;
+    var index = document.getElementById('index').value
     let json = {
         aggs: {
             minDate : {
@@ -54,9 +54,10 @@ function groupName(obj) {
 }
 
 function drawChart2(index, startDate, finishDate) {
-
+    const batchsize = 10000
+    const hits = []
     const json = {
-        size: 10000,
+        size: batchsize,
         query: {
             bool: {
             should: [
@@ -79,86 +80,122 @@ function drawChart2(index, startDate, finishDate) {
             minimum_should_match: 1
             }
         }
-    };
+    }
+
+    const handleResults = (sid, remaining, data) => {
+        data.forEach(h => {
+	    hits.push(h["_source"])
+	    remaining--
+	})
+	if(remaining !== 0) {
+	    scroll(sid, remaining)
+	} else {
+	    showTable(startDate, finishDate, hits)
+	}
+    }
+    
+    const scroll = (sid, remaining) => {
+	let json = {
+	    scroll: "1m",
+	    scroll_id: sid
+	}
+	$.ajax({
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            url: "http://localhost:9200/_search/scroll",
+            data: JSON.stringify(json)
+	}).done(results => {
+	    handleResults(results["_scroll_id"], remaining, results["hits"]["hits"])
+	}).fail((a,b,c) => {
+            console.log(b)
+            console.log(c)
+	})
+    }
     
     $.ajax({
         type: "POST",
         contentType: "application/json",
         dataType: "json",
-        url: "http://localhost:9200/" + index + "/_search",
+        url: "http://localhost:9200/" + index + "/_search?scroll=1m",
         data: JSON.stringify(json)
     }).done(results => {
-        const hits = results["hits"]["hits"].map(h => h["_source"])
-        const container = document.getElementById("visualization")
-        const groupNames0 = new Set()
-
-        hits.forEach(obj => {
-            groupNames0.add(groupName(obj))
-        });
-
-        const groupNames = Array.from(groupNames0).sort()
-        const groups = new vis.DataSet()
-	const groupMap = {}
-        for(let g = 0; g < groupNames.length; g++) {
-            groups.add({id: g, content: groupNames[g]})
-	    groupMap[groupNames[g]] = g
-        }
-
-        const colorMap = {}
-        colorMap["irods_capability_automated_ingest.sync_task.sync_file"] = 'sync_file';
-        colorMap["irods_capability_automated_ingest.sync_task.sync_dir"] = 'sync_dir';
-        colorMap["irods_capability_automated_ingest.sync_task.sync_path"] = 'sync_path';
-        colorMap["irods_capability_automated_ingest.sync_task.restart"] = 'restart';
-
-        const items = new vis.DataSet()
-	let count = 0
-        hits.forEach((obj, index) => {
-            let task_id = obj["event_id"]
-            let task_name = obj["event_name"]
-            let start=obj["start"]
-            let finish=obj["finish"]
-            let taskStartDate = new Date(start)
-            let taskEndDate = new Date(finish)
-            items.add({
-                id: index,
-                group: groupMap[groupName(obj)],
-                content: task_id,
-		title: `${task_id}<br/>start: ${taskStartDate}<br/>finish: ${taskEndDate}`,
-                start: taskStartDate,
-                end: taskEndDate,
-                className: colorMap[task_name]
-            })
-	    count++
-        });
-	document.getElementById("numEvents").innerHTML = count
-
-	let options = {
-	    tooltip: {
-		overflowMethod: "cap"
-	    },
-	    moveable: true,
-	    zoomable: true,
-	    selectable: false,
-	    showCurrentTime: false,
-	    stack: false,
-	    groupOrder: "content"
-	}
-	if(startDate !== undefined) {
-	    options["min"] = startDate
-	    options["start"] = startDate
-	    options["end"] = new Date(startDate.valueOf() + 1000)
-	}
-	if(finishDate !== undefined) {
-	    options["max"] = finishDate
-	}
-
-	container.innerHTML = ""
-        let timeline = new vis.Timeline(container)
-	timeline.setOptions(options)
-        timeline.setGroups(groups)
-        timeline.setItems(items)
+	const data = results["hits"]
+	const total = data["total"]
+	handleResults(results["_scroll_id"], total, data["hits"])
     }).fail((a,b,c) => {
         console.log(b)
         console.log(c)
-    });
+    })
+}
+
+function showTable(startDate, finishDate, hits){
+    const container = document.getElementById("visualization")
+    const groupNames0 = new Set()
+
+    hits.forEach(obj => {
+        groupNames0.add(groupName(obj))
+    })
+
+    const groupNames = Array.from(groupNames0).sort()
+    const groups = new vis.DataSet()
+    const groupMap = {}
+    for(let g = 0; g < groupNames.length; g++) {
+        groups.add({id: g, content: groupNames[g]})
+	groupMap[groupNames[g]] = g
+    }
+
+    const colorMap = {}
+    colorMap["irods_capability_automated_ingest.sync_task.sync_file"] = 'sync_file';
+    colorMap["irods_capability_automated_ingest.sync_task.sync_dir"] = 'sync_dir';
+    colorMap["irods_capability_automated_ingest.sync_task.sync_path"] = 'sync_path';
+    colorMap["irods_capability_automated_ingest.sync_task.restart"] = 'restart';
+
+    let count = hits.length
+    document.getElementById("numEvents").innerHTML = count
+    
+    const items = new vis.DataSet()
+    hits.forEach((obj, index) => {
+        let task_id = obj["event_id"]
+        let task_name = obj["event_name"]
+        let start=obj["start"]
+        let finish=obj["finish"]
+        let taskStartDate = new Date(start)
+        let taskEndDate = new Date(finish)
+        items.add({
+            id: index,
+            group: groupMap[groupName(obj)],
+            content: task_id,
+	    title: `${task_id}<br/>start: ${taskStartDate}<br/>finish: ${taskEndDate}`,
+            start: taskStartDate,
+            end: taskEndDate,
+            className: colorMap[task_name]
+        })
+    })
+
+    let options = {
+	tooltip: {
+	    overflowMethod: "cap"
+	},
+	moveable: true,
+	zoomable: true,
+	selectable: false,
+	showCurrentTime: false,
+	stack: false,
+	groupOrder: "content"
+    }
+    if(startDate !== undefined) {
+	options["min"] = startDate
+	options["start"] = startDate
+	options["end"] = new Date(startDate.valueOf() + 1000)
+    }
+    if(finishDate !== undefined) {
+	options["max"] = finishDate
+    }
+    
+    container.innerHTML = ""
+    let timeline = new vis.Timeline(container)
+    timeline.setOptions(options)
+    timeline.setGroups(groups)
+    timeline.setItems(items)
 }
