46217b8e2ff7263ac26539ce9300f1d6dfc00bd8
Jason Coposky <jasonc@renci.org>
2018-08-10 16:32:20 -0400

    [#25] remove debug code

--- irods_capability_automated_ingest/examples/register.py
+++ irods_capability_automated_ingest/examples/register.py
@@ -3,10 +3,6 @@ from irods_capability_automated_ingest.utils import Operation
 
 class event_handler(Core):
     @staticmethod
-    def to_resource(session, meta, **options):
-        return "pt"
-
-    @staticmethod
     def operation(session, meta, **options):
         return Operation.REGISTER_SYNC
 
