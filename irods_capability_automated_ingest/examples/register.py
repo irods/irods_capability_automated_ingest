1a52fbc7a3d01c72d3b23f333511836328f58b06
Jason Coposky <jasonc@renci.org>
2018-08-10 12:35:48 -0400

    [#25] fixes for link updates

--- irods_capability_automated_ingest/examples/register.py
+++ irods_capability_automated_ingest/examples/register.py
@@ -2,6 +2,9 @@ from irods_capability_automated_ingest.core import Core
 from irods_capability_automated_ingest.utils import Operation
 
 class event_handler(Core):
+    @staticmethod
+    def to_resource(session, meta, **options):
+        return "pt"
 
     @staticmethod
     def operation(session, meta, **options):
