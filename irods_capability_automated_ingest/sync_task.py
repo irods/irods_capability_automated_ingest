0fbbf1e42c61bdca6d3536fc89a4d1b499ad4466
gabiiiiiii <gabriellemaxwell123@live.com>
2021-07-21 14:45:02 -0400

    [#155] added exceptions in place of continue for sync_path,
    added function calls and put helpers in respective classes

--- irods_capability_automated_ingest/sync_task.py
+++ irods_capability_automated_ingest/sync_task.py
@@ -18,6 +18,8 @@ import traceback
 from celery.signals import task_prerun, task_postrun
 from billiard import current_process
 
+class ContinueException(Exception):
+    pass
 
 @task_prerun.connect()
 def task_prerun(task_id=None, task=None, args=None, kwargs=None, **kw):
@@ -47,8 +49,7 @@ class IrodsTask(app.Task):
         config = meta["config"]
         job = sync_job.from_meta(meta)
         logger = sync_logging.get_sync_logger(config["log"])
-        logger.error('failed_task', task=meta["task"], path=meta["path"], job_name=job.name(
-        ), task_id=task_id, exc=exc, einfo=einfo, traceback=traceback.extract_tb(exc.__traceback__))
+        logger.error('failed_task', task=meta["task"], path=meta["path"], job_name=job.name(), task_id=task_id, exc=exc, einfo=einfo, traceback=traceback.extract_tb(exc.__traceback__))
         job.failures_handle().incr()
 
     def on_retry(self, exc, task_id, args, kwargs, einfo):
@@ -128,7 +129,6 @@ def restart(meta):
             job.reset()
             meta = meta.copy()
             meta["task"] = "sync_path"
-            #task_queue(meta["path_queue"]).add(sync_path, meta)
             meta['queue_name'] = meta["path_queue"]
             enqueue_task(sync_path, meta)
         else:
@@ -147,21 +147,170 @@ def restart(meta):
 
 @app.task(bind=True, base=IrodsTask)
 def sync_path(self, meta):
+
     syncer = scanner.scanner_factory(meta)
-    syncer.sync_path(self, meta)
+    path = meta["path"]
+    config = meta["config"]
+    logging_config = config["log"]
+    exclude_file_name = meta['exclude_file_name']
+    exclude_directory_name = meta['exclude_directory_name']
+
+    logger = sync_logging.get_sync_logger(logging_config)
+
+    file_regex = [re.compile(r) for r in exclude_file_name]
+    dir_regex = [re.compile(r) for r in exclude_directory_name]
+
+    try:
+        logger.info("walk dir", path=path)
+        meta = meta.copy()
+        meta["task"] = "sync_dir"
+        chunk = {}
+
+        itr = syncer.instantiate(meta)
+
+        if meta["profile"]:
+            profile_log = config.get("profile")
+            profile_logger = sync_logging.get_sync_logger(profile_log)
+            task_id = self.request.id
+
+            profile_logger.info("list_dir_prerun", event_id=task_id + ":list_dir", event_name="list_dir",
+                                hostname=self.request.hostname, index=current_process().index)
+            itr = list(itr)
+            if meta["profile"]:
+                profile_logger.info("list_dir_postrun", event_id=task_id + ":list_dir", event_name="list_dir",
+                                    hostname=self.request.hostname, index=current_process().index)
+
+        for obj in itr:
+            obj_stats = {}
+
+            try:
+                full_path, obj_stats = syncer.itr(meta, obj, obj_stats)
+            except ContinueException:
+                continue
+
+            # add object stat dict to the chunk dict
+            chunk[full_path] = obj_stats
+
+            # Launch async job when enough objects are ready to be sync'd
+            files_per_task = meta.get('files_per_task')
+            if len(chunk) >= files_per_task:
+                sync_files_meta = meta.copy()
+                sync_files_meta['chunk'] = chunk
+                sync_files_meta['queue_name'] = meta["file_queue"]
+                enqueue_task(sync_files, sync_files_meta)
+                chunk.clear()
+
+        if len(chunk) > 0:
+            sync_files_meta = meta.copy()
+            sync_files_meta['chunk'] = chunk
+            sync_files_meta['queue_name'] = meta["file_queue"]
+            enqueue_task(sync_files, sync_files_meta)
+            chunk.clear()
+
+    except Exception as err:
+        event_handler = custom_event_handler(meta)
+        retry_countdown = event_handler.delay(self.request.retries + 1)
+        max_retries = event_handler.max_retries()
+        raise self.retry(max_retries=max_retries, exc=err, countdown=retry_countdown)
+
+def sync_entry(self, meta, cls, datafunc, metafunc):
+    
+    syncer = scanner.scanner_factory(meta)
+
+    path=meta["path"]
+    target=meta["target"]
+    config=meta["config"]
+    logging_config=config["log"]
+    logger=sync_logging.get_sync_logger(logging_config)
+
+    event_handler=custom_event_handler(meta)
+    max_retries=event_handler.max_retries()
+
+    lock=None
+
+    logger.info("synchronizing " + cls + ". path = " + path)
+
+    if scanner.is_unicode_encode_error_path(path):
+        # encodes to utf8 and logs warning
+        abspath=os.path.abspath(path)
+        path=os.path.dirname(abspath)
+        utf8_escaped_abspath=abspath.encode('utf8', 'surrogateescape')
+        b64_path_str=base64.b64encode(utf8_escaped_abspath)
+        unicode_error_filename='irods_UnicodeEncodeError_' + \
+                    str(b64_path_str.decode('utf8'))
+
+        logger.warning(
+                    'sync_entry raised UnicodeEncodeError while syncing path:' + str(utf8_escaped_abspath))
+
+        meta['path'] = path
+        meta['b64_path_str'] = b64_path_str
+        meta['unicode_error_filename'] = unicode_error_filename
+        sync_key = str(b64_path_str.decode('utf8')) + ":" + target
+    else:
+        sync_key = path + ":" + target
+
+    try:
+        r = get_redis(config)
+        lock = redis_lock.Lock(r, "sync_" + cls + ":"+sync_key)
+        lock.acquire()
+
+        sync_time_handle = sync_time_key_handle(r, sync_key)
+        if not meta["ignore_cache"]:
+            sync_time = sync_time_handle.get_value()
+        else:
+            sync_time = None
+
+        mtime = meta.get("mtime")
+        if mtime is None:
+            mtime = getmtime(path)
+
+        ctime = meta.get("ctime")
+        if ctime is None:
+            ctime = getctime(path)
+
+        if sync_time is not None and mtime < sync_time and ctime < sync_time:
+            logger.info("succeeded_" + cls +
+                        "_has_not_changed", task=meta["task"], path=path)
+        else:
+            t = datetime.now().timestamp()
+            logger.info("synchronizing " + cls, path=path,
+                        t0=sync_time, t=t, ctime=ctime)
+            meta2 = meta.copy()
+            if path == meta["root"]:
+                if 'unicode_error_filename' in meta:
+                    target2 = join(target, meta['unicode_error_filename'])
+                else:
+                    target2 = target
+            else:
+                target2 = syncer.construct_path(meta2, path)
+
+            meta2["target"] = target2
+            if sync_time is None or mtime >= sync_time:
+                datafunc(event_handler.get_module(), meta2, logger, True)
+                logger.info("succeeded", task=meta["task"], path=path)
+            else:
+                metafunc(event_handler.get_module(), meta2, logger)
+                logger.info("succeeded_metadata_only",
+                            task=meta["task"], path=path)
+            sync_time_handle.set_value(str(t))
+    except Exception as err:
+        event_handler = custom_event_handler(meta)
+        retry_countdown = event_handler.delay(self.request.retries + 1)
+        raise self.retry(max_retries=max_retries,
+                             exc=err, countdown=retry_countdown)
+    finally:
+        if lock is not None:
+            lock.release()
 
 
 @app.task(bind=True, base=IrodsTask)
 def sync_dir(self, meta):
-    syncer = scanner.scanner_factory(meta)
-    syncer.sync_entry(self, meta, "dir", sync_irods.sync_data_from_dir,
+    sync_entry(self, meta, "dir", sync_irods.sync_data_from_dir,
                       sync_irods.sync_metadata_from_dir)
 
 
 @app.task(bind=True, base=IrodsTask)
 def sync_files(self, _meta):
-    #import here due to circular dependencies
-    from .scanner import scanner_factory
     chunk = _meta["chunk"]
     meta = _meta.copy()
     for path, obj_stats in chunk.items():
@@ -173,10 +322,8 @@ def sync_files(self, _meta):
         meta["ctime"] = obj_stats.get('ctime')
         meta["size"] = obj_stats.get('size')
         meta['task'] = 'sync_file'
-        syncer = scanner_factory(meta)
-        syncer.sync_entry(self, meta, "file", sync_irods.sync_data_from_file,
-                          sync_irods.sync_metadata_from_file)
-
+        sync_entry(self, meta, "file", sync_irods.sync_data_from_file, 
+            sync_irods.sync_metadata_from_file)
 
 # Use the built-in version of scandir/walk if possible, otherwise
 # use the scandir module version
